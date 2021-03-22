e41648661934d587ebee2a6f9b2a0f941f66f2e2
Lavrentev Stanislav <hvoinii@gmail.com>
2021-03-22 19:35:52 +0600

    install boostrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import "./assets/styles/main.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button variant="success">Success</Button>
+    </div>
+  );
 }
 
 export default App;
