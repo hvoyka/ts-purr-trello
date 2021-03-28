fee18493c5f444de51f057e4e482b01be57e4fa9
Lavrentev Stanislav <hvoinii@gmail.com>
2021-03-28 19:43:57 +0600

    FEAT: CardModal - add textareas for title and text; connect to changeCardTitle function

--- src/components/MainDesk/components/CardModal/CardModal.tsx
+++ src/components/MainDesk/components/CardModal/CardModal.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Button, Modal } from "react-bootstrap";
+import styled from "styled-components";
 
 interface CardModalProps {
   id: string;
@@ -35,18 +36,51 @@ const CardModal: React.FC<CardModalProps> = ({
   return (
     <>
       <Modal show={show} onHide={handleClose} centered={true}>
-        <Modal.Header>
-          <Modal.Title>{title}</Modal.Title>
+        <Modal.Header closeButton>
+          <TextArea
+            placeholder={"Card title"}
+            rows={1}
+            onChange={(e: any) => {
+              changeCardTitle(id, e.target.value);
+            }}
+          >
+            {title}
+          </TextArea>
         </Modal.Header>
         <Modal.Body>
-          <textarea></textarea>
+          <TextArea placeholder={"Card text"}>{text}</TextArea>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary">Comfirm</Button>
-        </Modal.Footer>
+        <Modal.Body>
+          <div>Comments</div>
+        </Modal.Body>
       </Modal>
     </>
   );
 };
 
 export default CardModal;
+
+const TextArea = styled.textarea`
+  overflow: hidden;
+  overflow-wrap: break-word;
+  resize: none;
+  background: transparent;
+  border-radius: 3px;
+  box-shadow: none;
+  font-weight: 600;
+  min-height: 20px;
+  padding: 4px 8px;
+  resize: none;
+  max-height: 256px;
+  width: 100%;
+  outline: none;
+  border: none;
+  -webkit-appearance: none;
+  border-radius: 3px;
+  display: block;
+  color: var(--blue2);
+  &:focus {
+    background-color: var(--white);
+    box-shadow: inset 0 0 0 2px var(--blue2);
+  }
+`;
