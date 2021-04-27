9155ba1a9c907a9d225844e8fe60d4c459c18f6c
Lavrentev Stanislav <hvoinii@gmail.com>
2021-04-27 11:50:29 +0600

    FEAT: add redux-persist code to store.ts and index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,21 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./assets/styles/colors.css";
+
+let persistor = persistStore(store);
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
 import userReducer from "./ducks/user/userSlice";
 import columnsReducer from "./ducks/columns/columnsSlice";
 import cardsReducer from "./ducks/cards/cardsSlice";
 
+const reducers = combineReducers({
+  columns: columnsReducer,
+  cards: cardsReducer,
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const store = configureStore({
-  reducer: {
-    columns: columnsReducer,
-    cards: cardsReducer,
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
